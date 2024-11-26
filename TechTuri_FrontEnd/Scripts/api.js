5674f7ee0979f424fcee9ebcb0c76ddccde58bc9
KingDyte <zeroskillz555@gmail.com>
2024-11-26 17:28:00 +0100

    .

--- TechTuri_FrontEnd/Scripts/api.js
+++ TechTuri_FrontEnd/Scripts/api.js
@@ -1,4 +1,4 @@
-var defaultUrl = "https://localhost:6200/api/";
+var defaultUrl = "https://localhost:5263/api/";
 
 async function postData(url = "", data = {}, needAuth = true) {
     // Default options are marked with *
--- TechTuri_FrontEnd/Scripts/auth.js
+++ TechTuri_FrontEnd/Scripts/auth.js
@@ -1,13 +1,11 @@
 async function login() {
-    var email = document.getElementById('typeEmail').value;
-    var password = document.getElementById('typePassword').value;
-    if (isEmpty(email) || isEmpty(password)) {
-        alert('Email és jelszó megadása kötelező!');
-    } else if (!validateEmail(email)) {
-        alert('Hibás email formátum!');
+    var uName = document.getElementById('uName').value;
+    var password = document.getElementById('pw').value;
+    if (isEmpty(uName) || isEmpty(password)) {
+        alert('Felhasználónév és jelszó megadása kötelező!');
     } else {
         var data = {
-            email: email,
+            uName: uName,
             password: password
         };
         await postData("auth/login", data, false)
@@ -25,9 +23,3 @@ async function login() {
 function isEmpty(str) {
     return (!str || 0 === str.length);
 }
-
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
